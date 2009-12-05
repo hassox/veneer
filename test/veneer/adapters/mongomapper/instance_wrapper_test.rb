b8c33cdd1b935ef8ff335024c2f71a33c36f663b
Daniel Neighman <has.sox@gmail.com>
2009-12-05 22:43:30 +1100

    And again I forget the file

---
+++ test/veneer/adapters/mongomapper/instance_wrapper_test.rb
@@ -0,0 +1,21 @@
+require File.join(File.dirname(__FILE__), "..", "..", "..", "test_helper")
+require File.join(File.dirname(__FILE__), "mongomapper_setup")
+
+module Veneer
+  module Test
+    class MongoMapperInstanceWrapper < ::Test::Unit::TestCase
+      context "MongoMapper Veneer Adapter" do
+        setup do
+          @klass = MongoFoo
+          @valid_attributes   = {:name => "foo"}
+          @invalid_attributes = {:name => "invalid"}
+        end
+
+        veneer_should_implement_new_record?
+        veneer_should_implement_save
+        veneer_should_implement_save!
+        veneer_should_implement_destroy
+      end
+    end
+  end
+end
