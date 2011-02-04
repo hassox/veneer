a3d7ce40e921c8fcf421af6b6c98fb7d077502b6
Maciej Lotkowski <maciej.lotkowski@gmail.com>
2011-02-04 17:19:03 +0100

    Cleanup

--- lib/veneer/adapters/datamapper/types.rb
+++ lib/veneer/adapters/datamapper/types.rb
@@ -5,22 +5,7 @@ module DataMapper
         class Types
           def self.normalize(type)
             case type
-            # when ::DataMapper::Property::Binary then :binary
-            # when ::DataMapper::Property::Boolean then :boolean
-            # when ::DataMapper::Property::Class then :string
-            # when ::DataMapper::Property::Date then :date
-            # when ::DataMapper::Property::DateTime then :datetime
-            # when ::DataMapper::Property::Decimal then :datetime
-            # when ::DataMapper::Property::Discriminator then :string
-            # when ::DataMapper::Property::Float then :float
             when ::DataMapper::Property::Integer then ::Integer
-            # when ::DataMapper::Property::Numeric then :float
-            # when ::DataMapper::Property::Object then :text
-            # when ::DataMapper::Property::Serial then :serial
-            # when ::DataMapper::Property::String then :string
-            # when ::DataMapper::Property::Text then :text
-            # when ::DataMapper::Property::Time then :time
-            # else :text
             else type
             end
           end
--- lib/veneer/adapters/mongomapper/types.rb
+++ lib/veneer/adapters/mongomapper/types.rb
@@ -5,19 +5,7 @@ module MongoMapper
         class Types
           def self.normalize(type)
             case type
-              # when ::MongoMapper::Extensions::Array then :serialized
-              # when ::MongoMapper::Extensions::Binary then :binary
-              # when ::MongoMapper::Extensions::Boolean then :boolean
-              # when ::MongoMapper::Extensions::Date then :date
-              # when ::MongoMapper::Extensions::Float then :float
-              # when ::MongoMapper::Extensions::Hash then :serialized
-              # when ::MongoMapper::Extensions::Integer then :integer
-              # when ::MongoMapper::Extensions::NilClass then :serialized
-              # when ::MongoMapper::Extensions::Object then :serialized
               when ::MongoMapper::Extensions::ObjectId then String
-              # when ::MongoMapper::Extensions::Set then :serialized
-              # when ::MongoMapper::Extensions::String then :string
-              # when ::MongoMapper::Extensions::Time then :time
               else type
             end
           end
